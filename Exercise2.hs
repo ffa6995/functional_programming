3c1172a7289ad6681982d35b2ac2e263113cc47f
Frederick-Zech <fze8406@students.fhv.at>
2021-09-16 18:00:25 +0200

    Exercise2

---
+++ Exercise2.hs
@@ -0,0 +1,17 @@
+n :: p -> Int
+n a = a `div` length xs
+ where
+  a = 10
+  xs = [1,2,3,4,5]
+
+mylast :: [a] -> a
+mylast ls = head (reverse ls)
+
+anotherlast :: [a] -> a
+anotherlast ls = ls !! subtract 1 (length ls)
+
+init' :: [a] -> [a]
+init' xs = tail (reverse(reverse xs))
+
+myinit :: [a] -> [a]
+myinit list = take (subtract 1 (length list)) list
