eacf37d90ce3efd1940971d1b6d7a7debf86f4a8
Fabian Falco <fabs.falco@hotmail.com>
2021-09-23 11:41:34 +0200

    add functions

--- Defining_Functions.hs
+++ Defining_Functions.hs
@@ -1,3 +1,4 @@
+-- 4.6.5 Implementing safetail
 -- Conditional Expression
 safetail :: [a] -> [a]
 safetail x = if null x then [] else tail x
@@ -11,3 +12,14 @@ safetail' x | null x = []
 safetail'' :: [a] -> [a]
 safetail'' [] = []
 safetail'' (_:xs) = xs
+
+-- 4.6.6 Defining Functions
+-- absolute between two numbers
+abs' :: (Num a, Ord a) => a -> a 
+abs' a = if a <= 0 then a * (-1) else a
+absDif :: Num a => a -> a -> a
+absDif a b = abs(a - b)
+
+-- halve list
+halve :: [a] -> ([a], [a])
+halve a = (reverse (drop (length a `div` 2) (reverse a)), drop (length a `div` 2) a)
