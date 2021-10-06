a641426473c04a3b046394e221a6a300d85405a3
Fabian Falco <fabs.falco@hotmail.com>
2021-10-06 15:05:25 +0200

    add additional recursive functions

--- Recursive_Functions.hs
+++ Recursive_Functions.hs
@@ -1,9 +1,29 @@
 -- and
+and' :: [Bool] -> Bool
+and' [] = True
+and' (x:xs) | x == True = and' xs
+            | x == False = False
 
 -- concat
+concat' :: [[a]] -> [a]
+concat' [] = []
+concat' ([]:vs) = concat' vs
+concat' ((x:xs):vs) = x:concat' (xs:vs)
 
 -- replicate
+replicate' :: Int -> a -> [a]
+replicate' 0 x = []
+replicate' x y = y : replicate' (x-1) y
 
 -- (!!) nth element of a list
+elementN :: [a] -> Int -> a
+elementN [] y = error "index too large"
+elementN x 0 = head x
+elementN (x:xs) y = elementN xs (y-1)
 
 -- elem
+-- elem' :: Eq a => a -> [a] -> Bool
+
+
+-- merge sort
+-- merge :: Ord a => [a] -> [a] -> [a]
