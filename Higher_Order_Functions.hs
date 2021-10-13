6a0495ba08f5e1f9d03a897a4d56e6059e1a1d9f
Fabian Falco <fabs.falco@hotmail.com>
2021-10-13 21:39:22 +0200

    complete higher order functions

--- Higher_Order_Functions.hs
+++ Higher_Order_Functions.hs
@@ -26,12 +26,24 @@ pipeline'' f g h x = h (g (f x))
 
 -- 6.7.5 Decimals to Integer
 dec2int :: [Int] -> Int
-dec2int x  = foldl (\x y -> 10*x+y) 0 x
+dec2int = foldl (\x y -> 10*x+y) 0
 
 -- 6.7.6 Palindrome Checker
 palindrome :: String -> Bool
-palindrome [] = True 
-palindrome x = str == reverse(str)
-    where str = filter (\x -> x /= ' ') (map toLower x)
-    
+palindrome [] = True
+palindrome x = str == reverse str
+    where str = filter (/= ' ') (map toLower x)
 
+-- 6.7.7 Unfolding
+unfold :: (t -> Bool) -> (t -> a) -> (t -> t) -> t -> [a]
+unfold p h t x
+    | p x       = []
+    | otherwise = h x : unfold p h t (t x)
+
+-- redefine map f xs
+map' :: (a -> b) -> [a] -> [b]
+map' f = unfold null (f.head) tail
+
+-- redefine iterate f
+iterate' :: (a -> a) -> a -> [a]
+iterate' f x = unfold (const False) id f x
