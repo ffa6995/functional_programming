088375deb2074b46de5d19590f5235c8b38c9456
Fabian Falco <fazzer.crypot@hotmail.com>
2021-11-19 18:08:40 +0100

    add ziplist

--- TypeClasses.hs
+++ TypeClasses.hs
@@ -3,56 +3,58 @@ data Nat = Zero | Succ Nat
 
 -- Enum Instance
 instance Enum Nat where
-    toEnum 0 = Zero
-    toEnum x = Succ (toEnum (x-1))
-    fromEnum Zero = 0
-    fromEnum (Succ x) = fromEnum x+1
+  toEnum 0 = Zero
+  toEnum x = Succ (toEnum (x - 1))
+  fromEnum Zero = 0
+  fromEnum (Succ x) = fromEnum x + 1
 
 instance Eq Nat where
-    Zero == Zero = True
-    Succ a == Succ b = a == b
-    _    == _    = False
+  Zero == Zero = True
+  Succ a == Succ b = a == b
+  _ == _ = False
 
 -- Ord Instance
 instance Ord Nat where
-    Zero <= Zero = True
-    Zero <= Succ x = True
-    Succ x <= Zero = False
-    Succ x <= Succ y = x <= y
-
+  Zero <= Zero = True
+  Zero <= Succ x = True
+  Succ x <= Zero = False
+  Succ x <= Succ y = x <= y
 
 -- 9.14.2 Functorial Tree
-data Tree a = Leaf a 
-            | Branch (Tree a) a (Tree a)
-            deriving Show
+data Tree a
+  = Leaf a
+  | Branch (Tree a) a (Tree a)
+  deriving (Show)
 
 t :: Tree Int
-t = Branch (Branch (Leaf 10) 3 (Leaf 4))
-          5
-          (Branch (Leaf 6) 7 (Leaf 9))
+t =
+  Branch
+    (Branch (Leaf 10) 3 (Leaf 4))
+    5
+    (Branch (Leaf 6) 7 (Leaf 9))
 
 -- test with: fmap (+1) (Leaf 1)      /   fmap odd t
 instance Functor Tree where
-    fmap f (Leaf x) = Leaf (f x)
-    fmap f (Branch l x r) = Branch (fmap f l) (f x) (fmap f r)
+  fmap f (Leaf x) = Leaf (f x)
+  fmap f (Branch l x r) = Branch (fmap f l) (f x) (fmap f r)
 
 -- 9.14.3 ZipList
-newtype ZipList a = Z [a] deriving Show
+newtype ZipList a = Z [a] deriving (Show)
 
 instance Functor ZipList where
- -- fmap :: (a -> b) -> ZipList a -> ZipList b
- -- fmap g xs = g xs 
-  --fmap g (Z xs) = fmap g (Z xs)
-  
+  --fmap :: (a -> b) -> ZipList a -> ZipList b
+  fmap g (Z xs) = Z (fmap g xs)
+
 instance Applicative ZipList where
   --pure :: a -> ZipList a
-  --pure x = a -> ...
-  
- -- <*> :: ZipList (a -> b) -> ZipList a -> ZipList b
-  --(Z gs) <$> (Z xs) = ...
+  pure x = Z (repeat x)
+
+  --(<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
+  (Z gs) <*> (Z xs) = Z (zipWith (\f x -> f x) gs xs)
 
 -- Monoids
 -- 9.14.4.1 Boolean Monids
+
 
 -- 9.14.4.2 Maybe Monoid
 
