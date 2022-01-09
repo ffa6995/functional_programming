95f18785dd6f8d6f8f9b931101c017e9ab7cede7
Fabian Falco <fazzer.crypot@hotmail.com>
2022-01-09 16:39:45 +0100

    nim

--- 14_Monads_In_Use/Nim.hs
+++ 14_Monads_In_Use/Nim.hs
@@ -7,15 +7,56 @@ board :: [Int]
 board = [5,4,3,2,1]
 
 boardToString :: [Int] -> String
-boardToString board = boardToStringRow board
+boardToString board = boardToStringRow board 1
     where
-        boardToStringRow [] = ""
-        boardToStringRow (x:xs) = show x ++ ": " ++ concat (replicate x "* ") ++ if null xs then "" else "\n" ++ boardToStringRow xs 
+        boardToStringRow [] r = ""
+        boardToStringRow (x:xs) r = show r ++ ": " ++ concat (replicate x "* ") ++ if null xs then "" else "\n" ++ boardToStringRow xs (r+1)
 
-logic :: [Int] -> Int -> Int -> Maybe []
+logic :: [Int] -> Int -> Int -> Maybe [Int]
 logic b r s = if r > length b || r <= 0 || s <= 0 || b !! (r-1) < s then Nothing
                 else Just (removeStarsFromRow b r s 0)
                     where
                         removeStarsFromRow [] _ _ _ = []
-                        removeStarsFromRow (x:xs) r s cr = if cr == (r-1) then (x-1) : removeStartsFromRow xs r s (cr+1)
+                        removeStarsFromRow (x:xs) r s cr = if cr == (r-1) then (x-1) : removeStarsFromRow xs r s (cr+1)
                                                              else x : removeStarsFromRow xs r s (cr+1)
+
+changePlayer :: Player -> Player
+changePlayer One = Two
+changePlayer Two = One
+
+gameFinished :: [Int] -> Bool
+gameFinished [] = True
+gameFinished x = False
+
+nim :: IO ()
+nim = do
+    putStrLn "Start Nim"
+    turn board One
+    
+    
+turn :: [Int] -> Player -> IO ()
+turn b p = do
+    putStrLn ("Player " ++ show p ++ "'s turn")
+    putStrLn (boardToString board)
+    putStrLn "Pick one of the rows"
+    row <- getLine
+    putStrLn "How many stars you want to remove?"
+    stars <- getLine
+    let updatedBoard = logic b (read row) (read stars)
+    case updatedBoard of
+        Nothing -> do
+            putStrLn "You can't do this, try something else ;)"
+            turn b p
+        (Just board) -> do
+            if gameFinished board
+                then do
+                    putStrLn ("Player " ++ show p ++ " is the winner!")
+                    putStrLn "Next game is starting . . ."
+                    nim
+                else
+                    turn board (changePlayer p)
+
+main :: IO ()
+main = nim
+            
+    
